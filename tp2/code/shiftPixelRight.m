eb3ad28dccffca71cd62db30fbecd737449c0bee
Pier-Luc Auger <auger.pierluc@gmail.com>
2015-02-13 14:34:51 -0500

    sf

--- tp2/code/shiftPixelRight.m
+++ tp2/code/shiftPixelRight.m
@@ -1,28 +1,35 @@
 function [image] = shiftPixelRight(image,rowIndex,columnIndex,logging)
-  if logging
-    fprintf('\nTrying rowIndex: %d, columnIndex: %d',rowIndex,columnIndex);
-  end
+if logging
+fprintf('\nTrying rowIndex: %d, columnIndex: %d',rowIndex,columnIndex);
+end
+out=image;
+
+channels=size(image,3);
+
+RGB={channels};
+lines={channels};
   
-  out=image;
-  RGB={3};
-  RGB{1}=image(:,:,1);
-  RGB{2}=image(:,:,2);
-  RGB{3}=image(:,:,3);
+for i=1:channels
+  RGB{i}=image(:,:,i);
+end
 
-  lines={3};
-
-for i=1:3
+for i=1:channels
   lines{i} = RGB{i}(rowIndex,:);
   left = lines{i}(:,1:columnIndex-1);
   right = lines{i}(:,columnIndex+1:end);
   lines{i}=cat(2,0,left,right);
 end
 
-  allLines=cat(3,lines{1},lines{2},lines{3});
-  out(rowIndex,:,1)=allLines(:,:,1);
-  out(rowIndex,:,2)=allLines(:,:,2);
-  out(rowIndex,:,3)=allLines(:,:,3);
+if channels==3
+    allLines=cat(3,lines{1},lines{2},lines{3});
+    out(rowIndex,:,1)=allLines(:,:,1);
+    out(rowIndex,:,2)=allLines(:,:,2);
+    out(rowIndex,:,3)=allLines(:,:,3);
+else
+    allLines=cat(2,lines{1});
+    out(rowIndex,:,1)=allLines(:,:,1);
+end
 
-  image=out;
+image=out;
 
 end
