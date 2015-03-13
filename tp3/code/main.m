4b37b0417b1dfe9d4d17f5e9d28f8dcd1f9a2ab8
Pier-Luc Auger <auger.pierluc@gmail.com>
2015-03-13 10:11:53 -0400

    on avance

--- tp3/code/main.m
+++ tp3/code/main.m
@@ -1,19 +1,47 @@
+addpath('InPolygon-MEX/');
+
 save = false;
-pathToTxt = '/Users/plauger/git/PhotoAlgo/tp3/code/pic/06 - Pier-Luc.txt';
-pathToImage = '/Users/plauger/git/PhotoAlgo/tp3/code/pic/06 - Pier-Luc.png';
+pathToTxtFirst = '/Users/plauger/git/PhotoAlgo/tp3/code/pic/06 - Pier-Luc.txt';
+pathToImageFirst = '/Users/plauger/git/PhotoAlgo/tp3/code/pic/06 - Pier-Luc.png';
+pathToTxtSecond = '/Users/plauger/git/PhotoAlgo/tp3/code/pic/07 - SimonB.txt';
+pathToImageSecond = '/Users/plauger/git/PhotoAlgo/tp3/code/pic/07 - SimonB.png';
 
-image = imread(pathToImage);
-[ X,Y ] = readTxtFile(pathToTxt);
+image1 = imread(pathToImageFirst);
+[ X,Y ] = readTxtFile(pathToTxtFirst);
 
-
-% Déclarer des valeurs symboliques
+% symbolics
 syms tx ty theta sx sy
 
 % matrice de translation
 A1 = [1 0 tx; 0 1 ty; 0 0 1];
 
+% matrice de rotation
+A2 = [cos(theta) -sin(theta) 0; sin(theta) cos(theta) 0; 0 0 1];
+
+% matrice d'échelle
+A3 = [sx 0 0; 0 sy 0; 0 0 1];
+
+% essayons A1*A2*A3
+T1 = A1*A2*A3;
+
+
 %%Compute triangulation with Delaunay
 tri = delaunay(X,Y);
+figure(1);
+triplot(tri,X,Y);
+
+%find which triangle the pixel belongs to
+
+%loop for every triangle in triangulation
+for it=0:size(tri,1)
+    
+    
+    
+end
+
+
+%Interpolate
+%Vq = interp2(X,Y
 
 %%Calculate de morphed image
 %morphed_img = morph(img1, img2, img1_pts, img2_pts, tri, warp_frac, dissolve_frac);
