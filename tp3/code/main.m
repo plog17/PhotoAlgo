d0af22b95fa244ddbdf741367145ddb44e8972a4
Pier-Luc Auger <auger.pierluc@gmail.com>
2015-03-13 15:50:26 -0400

    can compute transformation on N pixels

--- tp3/code/main.m
+++ tp3/code/main.m
@@ -6,34 +6,24 @@ pathToImageFirst = '/Users/plauger/git/PhotoAlgo/tp3/code/pic/06 - Pier-Luc.png'
 pathToTxtSecond = '/Users/plauger/git/PhotoAlgo/tp3/code/pic/07 - SimonB.txt';
 pathToImageSecond = '/Users/plauger/git/PhotoAlgo/tp3/code/pic/07 - SimonB.png';
 
-image1 = imread(pathToImageFirst);
+image1 = im2double(imread(pathToImageFirst));
 [ X1,Y1 ] = readTxtFile(pathToTxtFirst);
-image2 = imread(pathToImageSecond);
+image2 = im2double(imread(pathToImageSecond));
 [ X2,Y2 ] = readTxtFile(pathToTxtSecond);
 
+%compute average X and Y
+for it=1:size(X1,1)
+   Xint(it) = (X1(it)+X2(it))/2;  
+   Yint(it) = (Y1(it)+Y2(it))/2;  
+end
 
-% symbolics
-syms tx ty theta sx sy
-% matrice de translation
-A1 = [1 0 tx; 0 1 ty; 0 0 1];
-% matrice de rotation
-A2 = [cos(theta) -sin(theta) 0; sin(theta) cos(theta) 0; 0 0 1];
-% matrice d'échelle
-A3 = [sx 0 0; 0 sy 0; 0 0 1];
-% essayons A1*A2*A3
-T1 = A1*A2*A3;
+%%Compute triangulation with Delaunay on the average X and Y
+tri = delaunay(Xint,Yint);
+figure(2);
+triplot(tri,Xint,Yint);
 
-
-%%Compute triangulation with Delaunay
-tri = delaunay(X1,Y1);
-figure(1);
-triplot(tri,X1,Y1);
-
-
-mesh=meshgrid(X1,Y1);
-Vq = interp2(mesh);
-
-
+%mesh=meshgrid(X1,Y1);
+%Vq = interp2(mesh);
 
 
 %loop for every triangle in triangulation
@@ -41,15 +31,29 @@ for it=1:size(tri,1)
     tri1 = [X1(tri(it,1)),Y1(tri(it,1));X1(tri(it,2)),Y1(tri(it,2));X1(tri(it,3)),Y1(tri(it,3))];
     tri2 = [X2(tri(it,1)),Y1(tri(it,1));X2(tri(it,2)),Y1(tri(it,2));X2(tri(it,3)),Y1(tri(it,3))];
 
-    intermediate = getIntermediateTriangle(tri1,tri2);
-    T = getTransformationMatrix(tri1,tri2);
-    
+    x=[0:size(image1,1)]';
+    y=[0:size(image1,2)]';
+    xs = [X1(tri(it,1));X1(tri(it,2));X1(tri(it,3))];
+   	ys = [Y1(tri(it,1));Y1(tri(it,2));Y1(tri(it,3))];
+    %find which triangle the pixels belongs to
+    [IN] = mytsearch(X1,Y1,tri,xs,ys);
+   
+    findedTri1 = [X1(IN(1)),Y1(IN(1));X1(IN(2)),Y1(IN(2));X1(IN(3)),Y1(IN(3))];
 
+    %intermediate = getIntermediateTriangle(tri1,tri2);
     
-    %find which triangle the pixel belongs to
-    [IN,ON,IN_strict] = InPolygon(px,py,cx,cy);
+    T1 = getTransformationMatrix(tri1,findedTri1);
+    T2 = getTransformationMatrix(tri2,findedTri1);
     
+    [xInTriangle,yInTriangle]=pointsInTriangle(findedTri1,image1);
     
+    %calculate TX=Y
+    unos = unos(1:size(xInTriangle,1));
+    X = vertcat(xInTriangle',yInTriangle',unos);
+ 
+    test = T1.*X;    
+    
+  %[INt,ON,IN_strict] = InPolygon(x,y,xs,ys);
 end
 
 
