c31835a58f9bc5ad186b9f5c5d1a55fd226259a6
Pier-Luc Auger <auger.pierluc@gmail.com>
2015-03-20 10:49:04 -0400

    return pts1 & pts2

--- tp4/code/matchPicturesPoints.m
+++ tp4/code/matchPicturesPoints.m
@@ -1,4 +1,4 @@
-function pts = matchPicturesPoints(image1path,image2path,fileToSave,nbPairs)
+function [pts1,pts2] = matchPicturesPoints(image1path,image2path,fileToSave,nbPairs)
     im1 = im2double(imread(image1path));
     im2 = im2double(imread(image2path));
 
@@ -6,4 +6,16 @@ function pts = matchPicturesPoints(image1path,image2path,fileToSave,nbPairs)
     subplot(1,2,2), image(im2), axis on, title(strcat('Image 2'));
 
     pts=ginput(nbPairs*2);
+    
+    pts1{nbPairs}=[];
+    pts2{nbPairs}=[];
+    
+    for i=1:2*nbPairs
+        if mod(i,2)==0
+           pts2{i/2}=pts(i,:);
+        else
+           pts1{ceil(i/2)}=pts(i,:);
+        end
+    end
+    
 end
