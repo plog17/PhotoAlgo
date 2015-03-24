74671784a84da4dd5351a30c436426aedf1a9b37
Pier-Luc Auger <auger.pierluc@gmail.com>
2015-03-24 18:38:48 -0400

    unstable

--- tp4/code/warpImage.m
+++ tp4/code/warpImage.m
@@ -11,28 +11,36 @@ function [ imwarped ] = warpImage( im,H )
     maxX=ceil(max([topLeft(1) topRight(1) bottomLeft(1) bottomRight(1)]));
     maxY=ceil(max([topLeft(2) topRight(2) bottomLeft(2) bottomRight(2)]));
     
+    
+    
+%im = rgb2gray(im);
 
 % size of the image
 szIm = size(im);
-
+size2=szIm(1,1:2);
 % default gray value
 grayDefault = 127;
+
+
 
 
 % pixel coordinates in original image
 [x,y] = meshgrid(1:size(im,2),1:size(im,1));
 
 % pixel coordinates in result image
-if any(szIm ~= size(im))
-[xp,yp] = meshgrid(1:szIm(2),1:szIm(1));
-pix   = [xp(:)'; yp(:)'];
-else
 pix   = [x(:)'; y(:)'];
-end
+
+%if any(szIm ~= size(im))
+%[xp,yp] = meshgrid(1:szIm(2),1:szIm(1));
+%pix   = [xp(:)'; yp(:)'];
+%else
+%pix   = [x(:)'; y(:)'];
+%end
+
 
 % homogeneous pixels in result frame.
-t=ones(1,prod(szIm));
-hPixels = [ pix'; t];
+filler=ones(2,prod(szIm));
+hPixels = [ pix; filler];
 
 % corresponding warped points in original frame
 hScene  = H*hPixels;
@@ -43,10 +51,17 @@ yprime=(hScene(2,:)./(hScene(3,:)))';
 xprime = reshape(xprime, szIm);
 yprime = reshape(yprime, szIm);
 
+% truc de pro: vectoriser l'image en un vecteur de Nx3 (N = # de pixels)
+imgVec = reshape(im, size(im,1)*size(im,2), size(im,3));
+
 % Warping an image according to the homography
-result = interp2(x,y,im,xprime,yprime, '*linear');
+result = interp2(x,y,imgVec,xprime,yprime);%,'linear',grayDefault); %, '*linear'
 result(isnan(result)) = grayDefault;
-result = reshape(result,szIm);
-    
+result = reshape(result,size2);
+
+% re-convertissons en format "image"
+imgWb = reshape(imgVec, size(im,1), size(im,2), size(im,3));
+
+
 end
 
