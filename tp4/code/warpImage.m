a9d42811c6d9f994aaab77267b3d909ac0fb8c4a
Pier-Luc Auger <auger.pierluc@gmail.com>
2015-03-24 20:00:13 -0400

    picture all blue

--- tp4/code/warpImage.m
+++ tp4/code/warpImage.m
@@ -11,42 +11,22 @@ function [ imwarped ] = warpImage( im,H )
     maxX=ceil(max([topLeft(1) topRight(1) bottomLeft(1) bottomRight(1)]));
     maxY=ceil(max([topLeft(2) topRight(2) bottomLeft(2) bottomRight(2)]));
     
+    % truc de pro: vectoriser l'image en un vecteur de Nx3 (N = # de pixels)
+    imgVec = reshape(im, size(im,1)*size(im,2), size(im,3));
+    %points=reshape(points, [ys,3,xs]);
+    %for(x=1:xs)
+    %    points(:,:,x)=points(:,:,x)*H;
+    %end
+    
+    imgVec = imgVec * H;
+    
+    %points=reshape(points, [ys,xs,3]);
+    % re-convertissons en format "image"
+    imgWb = reshape(imgVec, size(im,1), size(im,2), size(im,3));
 
-% size of the image
-szIm = size(im);
-
-% default gray value
-grayDefault = 127;
-
-
-% pixel coordinates in original image
-[x,y] = meshgrid(1:size(im,2),1:size(im,1));
-
-% pixel coordinates in result image
-if any(szIm ~= size(im))
-[xp,yp] = meshgrid(1:szIm(2),1:szIm(1));
-pix   = [xp(:)'; yp(:)'];
-else
-pix   = [x(:)'; y(:)'];
-end
-
-% homogeneous pixels in result frame.
-t=ones(1,prod(szIm));
-hPixels = [ pix'; t];
-
-% corresponding warped points in original frame
-hScene  = H*hPixels;
-
-% pixel coords in original frame.
-xprime=(hScene(1,:)./(hScene(3,:)))';
-yprime=(hScene(2,:)./(hScene(3,:)))';
-xprime = reshape(xprime, szIm);
-yprime = reshape(yprime, szIm);
-
-% Warping an image according to the homography
-result = interp2(x,y,im,xprime,yprime, '*linear');
-result(isnan(result)) = grayDefault;
-result = reshape(result,szIm);
+    outIm(:,:,1) = interp2(image(:,:,1), points(:,:,1), points(:,:,2));
+    outIm(:,:,2) = interp2(image(:,:,2), points(:,:,1), points(:,:,2));
+    outIm(:,:,3) = interp2(image(:,:,3), points(:,:,1), points(:,:,2));
     
 end
 
