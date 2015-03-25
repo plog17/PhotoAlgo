43182986b1c280480e508fbfed387227e441a8d6
Pier-Luc Auger <auger.pierluc@gmail.com>
2015-03-24 20:35:53 -0400

    H2 test

--- tp4/code/computeH.m
+++ tp4/code/computeH.m
@@ -24,5 +24,14 @@ function [ H ] = computeH( im1_pts, im2_pts )
     %3x3 with last element = 1
     H = [h(1) h(2) h(3); h(4) h(5) h(6); h(7) h(8) 1];
 
+    
+numPts = size(im1_pts,1);
+
+pts1=[im1_pts ones(numPts,1)];
+pts2=[im2_pts ones(numPts,1)];
+
+H2=pts1\pts2;
+    
+    
 end
 
--- tp4/code/warpImage.m
+++ tp4/code/warpImage.m
@@ -1,32 +1,30 @@
-function [ imwarped ] = warpImage( im,H )
-    [height width channels]=size(im);
-    
-    %corners
-    topLeft=[1 1 1]/H;
-    topRight=[width 1 1]/H;
-    bottomLeft=[1 height 1]/H;
-    bottomRight=[width height 1]/H;
-    minX=floor(min([topLeft(1) topRight(1) bottomLeft(1) bottomRight(1)]));
-    minY=floor(min([topLeft(2) topRight(2) bottomLeft(2) bottomRight(2)]));
-    maxX=ceil(max([topLeft(1) topRight(1) bottomLeft(1) bottomRight(1)]));
-    maxY=ceil(max([topLeft(2) topRight(2) bottomLeft(2) bottomRight(2)]));
-    
-    % truc de pro: vectoriser l'image en un vecteur de Nx3 (N = # de pixels)
-    imgVec = reshape(im, size(im,1)*size(im,2), size(im,3));
-    %points=reshape(points, [ys,3,xs]);
-    %for(x=1:xs)
-    %    points(:,:,x)=points(:,:,x)*H;
-    %end
-    
-    imgVec = imgVec * H;
-    
-    %points=reshape(points, [ys,xs,3]);
-    % re-convertissons en format "image"
-    imgWb = reshape(imgVec, size(im,1), size(im,2), size(im,3));
+function [ output ] = warpImage( im,H )
 
-    outIm(:,:,1) = interp2(image(:,:,1), points(:,:,1), points(:,:,2));
-    outIm(:,:,2) = interp2(image(:,:,2), points(:,:,1), points(:,:,2));
-    outIm(:,:,3) = interp2(image(:,:,3), points(:,:,1), points(:,:,2));
+% Create a list of all pixels' coordinates in output image
+    [x,y] = meshgrid(1:size(im,2),1:size(im,1));
+
+    % Create list of all row coordinates and column coordinates in separate
+    % vectors, x and y, including offset
+    x = reshape(x,1,[]) ;%- col_offset;
+    y = reshape(y,1,[]) ;%- row_offset;
     
+    % Create homogeneous coordinates for each pixel in output image
+    pan_pts(1,:) = y;
+    pan_pts(2,:) = x;
+    pan_pts(3,:) = ones(1,size(pan_pts,2));
+    
+    % Perform inverse warp to compute coordinates in current input image
+    
+    image_coords = H\pan_pts;
+    
+    row_coords = reshape(image_coords(1,:),size(im,2), size(im,1));
+    col_coords = reshape(image_coords(2,:),size(im,2), size(im,1));
+    
+    %interpolate color values
+    pixel_color_r = interp2(im(:,:,1), col_coords, row_coords, 'linear', 0);
+    pixel_color_g = interp2(im(:,:,2), col_coords, row_coords, 'linear', 0);
+    pixel_color_b = interp2(im(:,:,3), col_coords, row_coords, 'linear', 0);
+        
+    t=cat(3,pixel_color_r,pixel_color_g,pixel_color_b);
 end
 
