6617fbfd52f2a0c97c17c5a235dda7c2e4dd3122
Pier-Luc Auger <auger.pierluc@gmail.com>
2015-03-29 10:46:47 -0400

    copy

--- tp4/code/ransac.m
+++ tp4/code/ransac.m
@@ -1,41 +1,28 @@
 function [outH, bestError] = ransac(im1pts, im2pts, maxIt, permittedError)
-%RANSAC performs RANSAC to further remove outliers then compute H.
-% Also returns how many points were ultimately used. 
 
-bestModel;
-bestPoints;
+    numMatches=0;
+    ptsUsed=[];
 
-bestNumMatches=0;
-bestError=inf;
+    pts11=[im1pts ones(size(im1pts,1),1)];
 
-for i=1:maxIt
-    %choisir des points au hazard
-    randomPts= randperm(size(im1pts,1),4); 
-    src_pts = im1pts(randomPts,:);
-    dest_pts = im2pts(randomPts,:);
-    
-    H_temp = computeH(src_pts, dest_pts);
-    
-    % Avoir Nx3 pour match des dimensions
-    im1pts_1=cat(2,im1pts,ones(size(im1pts,1),1));
-     
-    % appliquer l'homographie
-    testsPts = im1pts_1 * H_temp;
-    
-    % tester la qualité de l'homographie en évaluant l'erreur
-    currentError = (im2pts(:,1)-testsPts(:,1)).^2 + (im2pts(:,2)-testsPts(:,2)).^2;
-    
-    [x,~,~] = find(currentError < permittedError);
-    currentNbMatches = size(x,1);
+    for i=1:maxIt 
+        currentPts= randperm(size(im1pts,1),4);
 
-    if currentNbMatches > bestNumMatches
-        fprintf('%d',currentNbMatches);
-        if currentError < bestError
-            outH = H_temp;
-            bestNumMatches = currentNbMatches;
-            bestError=currentError;
-        end
+        H=computeH(im1pts(currentPts,:), im2pts(currentPts,:));
+
+
+        ptsTmp=pts11*H;
+
+        ds=(im2pts(:,1)-ptsTmp(:,1)).^2 + (im2pts(:,2)-ptsTmp(:,2)).^2;
+
+        ds=[ds [1:size(im1pts,1)]'];
+        ds(ds(:,1)>permittedError,:)=[];
+
+        if size(ds,1)>numMatches 
+            numMatches=size(ds,1);
+            ptsUsed=ds(:,2);
+            end
     end
-end
 
+    outH=computeH(im1pts(ptsUsed,:), im2pts(ptsUsed,:));
 end
