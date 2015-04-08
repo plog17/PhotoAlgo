08b7502bc0d39e63d7666941baeef02a19561856
Pier-Luc Auger <auger.pierluc@gmail.com>
2015-04-07 21:36:28 -0400

    trying matrix

--- tp5/code/getImageFromPath.m
+++ tp5/code/getImageFromPath.m
@@ -1,14 +1,14 @@
-function [ images_array, images_logExposure ] = getImageFromPath( path )
-    images = dir(strcat(path,'*.jpg'));
-    imagesIndex = find(~[images.isdir]);
-    images_array{size(images,1)}=[];
-    images_logExposure{size(images,1)}=[];
-
-    for imageIndex = 1:length(imagesIndex)
-        images_array{imageIndex}=im2double(imread(strcat(path,images(imagesIndex(imageIndex)).name)))*1000;
-        infos=imfinfo(strcat(path,images(imagesIndex(imageIndex)).name));
-        logExposure=log(infos.DigitalCamera.ExposureTime);
-        images_logExposure{imageIndex}=logExposure;
+function [ images, images_logExposure ] = getImageFromPath( path ) 
+    images = dir(strcat(path,'*.jpg')); 
+    imagesIndex = find(~[images.isdir]); 
+    sampleImage=imread(strcat(path,images(imagesIndex(1)).name)); 
+    images=zeros(size(sampleImage,1),size(sampleImage,2),3,size(imagesIndex,2)); 
+    images_logExposure=zeros(size(images,1),1); 
+    
+    for imageIndex = 1:length(imagesIndex) 
+        images(:,:,:,imageIndex)=round(im2double(imread(strcat(path,imagesIndex(imageIndex).name)))*1000); 
+        infos=imfinfo(strcat(path,images(imagesIndex(imageIndex)).name)); 
+        logExposure=log(infos.DigitalCamera.ExposureTime); 
+        images_logExposure(imageIndex,1)=logExposure; 
     end
 end
-
