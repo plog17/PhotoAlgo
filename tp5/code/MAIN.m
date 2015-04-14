2ffb456411c7c084e6503a7793fa12443d5b92fa
Pier-Luc Auger <auger.pierluc@gmail.com>
2015-04-13 20:45:19 -0400

    clean main

--- tp5/code/MAIN.m
+++ tp5/code/MAIN.m
@@ -1,9 +1,6 @@
 path='../web/images/origin/sphere/';
 path3='../web/images/perso3/sphere/';
-numPixels=250;
-srow=20;
-scol=40;
-channels=3;
+
 
 addpath('tests/program/')
 addpath('radiance/')
@@ -11,10 +8,15 @@ addpath('radiance/')
 
 %% Carte de radiance
 im=GenerateHDR(path3,'perso3');
+imshow(im);
 im2=GenerateHDR(path,'origin');
 
 %% Transormation panoramique
 latlon = generateEquiRectImage(im);
 
 %% Composition
-final = compose('../web/images/perso3/blender/');
+%facteur d'illumination
+c=5;
+final = compose('../web/images/perso3/blender/',c);
+imshow(final);
+imwrite(final,'final.jpg');
