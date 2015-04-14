d099d4a2925ebb442c3888eaa8da1a7f4e8e6d5b
Pier-Luc Auger <auger.pierluc@gmail.com>
2015-04-13 20:45:06 -0400

    compose ok

--- tp5/code/compose.m
+++ tp5/code/compose.m
@@ -1,9 +1,12 @@
-function [ final ] = compose( path )
+function [ final ] = compose( path,c)
+    %c=facteur d'illumination
+
     E = im2double(imread(strcat(path,'rendered_empty.png')));
     M = im2double(imread(strcat(path,'rendered_mask.png')));
     R = im2double(imread(strcat(path,'rendered.png')));
-
-   composite = M.*R + (1-M).*I + (1-M).*(R-E).*c
+    I = im2double(imread(strcat(path,'background.jpg')));
     
+    
+    final = M.*R + (1-M).*I + (1-M).*(R-E).*c;
 end
 
